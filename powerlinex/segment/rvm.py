36b03ebd476b0d020af72d8e65655b3ac41b3331
Emily Hyland <price.emily@gmail.com>
2013-12-06 00:09:56 -0800

    Initial commit

---
+++ powerlinex/segment/rvm.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+from subprocess import Popen, PIPE
+from powerline.theme import requires_segment_info
+
+
+def readlines(cmd, cwd):
+    p = Popen(cmd, shell=False, stdout=PIPE, stderr=PIPE, cwd=cwd)
+    p.stderr.close()
+    with p.stdout:
+        for line in p.stdout:
+            yield line[:-1].decode('utf-8')
+
+
+@requires_segment_info
+def version(pl, segment_info):
+    try:
+      for line in readlines(["rvm", "current"], segment_info['getcwd']()):
+          # Now to process line
+          if line == "system":
+              return None
+          else:
+              ret = []
+              info = line.split('@')
+              if len(info) > 1:
+                  ret.append({
+                      'contents': info[1],
+                      'highlight_group': ['ruby_version', 'virtualenv']
+                  })
+              version = info[0]
+              info = version.split('-', 1)
+              if info[0] == 'ruby':
+                  ret.insert(0, {
+                      'contents': info[1],
+                      'highlight_group': ['ruby_version', 'virtualenv']
+                  })
+              else:
+                  ret.prepend({
+                      'contents': version,
+                      'highlight_group': ['ruby_version', 'virtualenv']
+                  })
+              return ret
+    except OSError as e:
+        if e.errno == 2:
+            pass
+        else:
+            raise
